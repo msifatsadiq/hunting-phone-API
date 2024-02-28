335c3716ebe81de7436870aebdc388cdc4851a3a
msifatsadiq <msifatsadiq@gmail.com>
2024-02-29 00:42:27 +0600

    adding the phones container using api

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,30 @@
-console.log('hello')
+const loadData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json()
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones)
+}
+
+const displayPhones = phones => {
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const phonesDiv = document.createElement('div');
+        phonesDiv.classList = `card w-96 bg-base-100 shadow-xl`;
+        phonesDiv.innerHTML = `
+    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+    <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+    </div>
+    `;
+        phonesContainer.appendChild(phonesDiv);
+    });
+
+}
+
+loadData()
